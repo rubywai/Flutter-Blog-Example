87f29f774b5d1e0e765d83bdcce79aa2dc5d74fd
Ruby Wai <rubyland.myanmar.mg@gmail.com>
2022-04-05 13:44:41 +0630

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rest_api_blog/data/api_service/post_api_service.dart';
 import 'package:flutter_rest_api_blog/screen/home.dart';
+import 'package:flutter_rest_api_blog/utils/locale.dart';
 import 'package:flutter_rest_api_blog/widget/bottom_nav.dart';
 import 'package:get/get.dart';
 void main() {
@@ -13,8 +14,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      home: BottomNav(),
+    return  GetMaterialApp(
+      translations: RestApiLanguage(),
+      locale: const Locale('en',"US"),
+      fallbackLocale: const Locale('en',"US"),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: const BottomNav(),
       
     );
   }
--- lib/screen/setting_screen.dart
+++ lib/screen/setting_screen.dart
@@ -1,14 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class Setting extends StatelessWidget {
+class Setting extends StatefulWidget {
   const Setting({ Key? key }) : super(key: key);
 
   @override
+  State<Setting> createState() => _SettingState();
+}
+
+class _SettingState extends State<Setting> {
+  bool _darkTheme = false;
+  bool _mmLanguage = false;
+  @override
   Widget build(BuildContext context) {
+    Brightness brightness = Theme.of(context).brightness;
+    _darkTheme = (brightness == Brightness.dark);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text('Setting'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            child: ListTile(
+              leading: Text('Dark Theme'),
+              trailing: Switch(value: _darkTheme, 
+              onChanged: (value){
+                if(value){
+                  Get.changeTheme(ThemeData.dark());
+                }
+                else{
+                  Get.changeTheme(ThemeData.light());
+                }
+                setState(() {
+                   _darkTheme = value;
+                });
+                 
+              }),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Text('မြန်မာဘာသာသို့ပြောင်းရန်'),
+              trailing: Switch(value: _mmLanguage , 
+              onChanged: (val){
+                if(val){
+                  Get.updateLocale(Locale('en','MM'));
+                }
+                else {
+                  Get.updateLocale(Locale('en','US'));
+                }
+                setState(() {
+                  _mmLanguage = val;
+                });
+              }),
+            ),
+          )
+        ],
       ),
       
     );
